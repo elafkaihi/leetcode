af4061eb6964d699ecdb340f3eb8ede93ee4edc5
ElMehdiiiii <elmehdi.lafkaihi@centrale-casablanca.ma>
2024-02-18 17:05:58 +0100

    day one

---
+++ merge_sorted_array.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        if nums2  == [] : 
+            return None
+        i = m-1
+        j = n-1
+        k = n+m-1
+        while j >= 0 :
+            if nums2[j] < nums1[i] and i >=0: 
+                nums1[k] = nums1[i]
+                i -= 1 
+            else : 
+                nums1[k] = nums2[j]
+                j -= 1   
+            k -= 1               
+        
