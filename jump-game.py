2a90f9d1dba774e2efbc1eab92477cab2d2990ce
El Mehdi Lafkaihi <115099306+ElMehdiiiii@users.noreply.github.com>
2024-05-12 10:09:43 +0100

    Create jump-game.py

---
+++ jump-game.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        goal = len(nums) - 1
+
+        for i in range(len(nums)-1, -1,-1):
+            if i+nums[i] >= goal:
+                goal = i
+        
+        if goal == 0:
+            return True
+        return False
