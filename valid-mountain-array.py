cdd6446f302d4f0efda0fb88329d697a5739ceb7
elafkaihi <elmehdi.lafkaihi@centrale-casablanca.ma>
2024-08-09 12:26:03 +0100

    valid-mountain-array

---
+++ valid-mountain-array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        i = 1
+        if len(arr) < 2:
+            return False 
+        while i < len(arr) and arr[i] > arr[i-1]:
+            i += 1 
+        if i==1 or i==len(arr):
+            return False
+        while i < len(arr) and arr[i] < arr[i-1]:
+            i += 1
+        return i==len(arr)
